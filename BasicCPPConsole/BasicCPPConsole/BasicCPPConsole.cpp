afde04f905cafe352d3af151b319219ea67b6333
JoaBories <joa.bories@artfx.fr>
2025-03-24 15:40:12 +0100

    Clean code and more function

--- BasicCPPConsole/BasicCPPConsole/BasicCPPConsole.cpp
+++ BasicCPPConsole/BasicCPPConsole/BasicCPPConsole.cpp
@@ -1,6 +1,3 @@
-// BasicCPPConsole.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <vector>
 #include <map>
@@ -43,23 +40,58 @@ int RollAbility()
     const size_t numberOfRolls = 4;
     int rolls[numberOfRolls];
     int minimumRoll = 18;
-    int minimumRollIndex = 0;
+    size_t minimumRollIndex = 0;
 
     for (size_t i = 0; i < 4; i++)
     {
         rolls[i] = ((rand() % 6) + 1) * 3;
-        cout << rolls[i];
+
         if (rolls[i] < minimumRoll) 
         {
             minimumRoll = rolls[i];
             minimumRollIndex = i;
         }
     }
-    JumpLine();
 
     rolls[minimumRollIndex] = 0;
 
     return Sum(rolls, numberOfRolls);
+}
+
+void DisplayBagArray(string* bag, const size_t size) 
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        if (bag[i] != "")
+        {
+            cout << "\t -" << bag[i] << endl;
+        }
+    }
+}
+
+void DisplayBagVector(vector<string> bag)
+{
+    for (string item : bag)
+    {
+        cout << "\t -" << item << endl;
+    }
+}
+
+int AskForInt(string firstQ, string secondQ, int maxAnswer) //  1 - Yes / 2 - No : maxAnswer = 2
+{
+    int playerAnswer;
+
+    cout << firstQ << endl;
+
+    cin >> playerAnswer;
+
+    while (playerAnswer > maxAnswer || playerAnswer < 1)
+    {
+        cout << secondQ << endl;
+        cin >> playerAnswer;
+    }
+
+    return playerAnswer;
 }
 
     
@@ -67,15 +99,37 @@ int RollAbility()
 
 int main()
 {
+
+#pragma region Variables
+
     srand(time(NULL));
 
     string narrator = "The Almighty";
     string separator = "\n---------------------------------------------\n";
 
+    string playerName;
+    char playerClass;
+
+    map<string, int> abilities{
+        {"strength", RollAbility()},
+        {"dexterity", RollAbility()},
+        {"constitution", RollAbility()},
+        {"intelligence", RollAbility()}
+    };
+
+    string smallbag[5] = { "Sword", "Shield", "Bow" };
+    vector<string> magicBag = { "Magic Hat", "Magic Wand", "Magic Cape" };
+
+    string playerAnswerStr;
+    int playerAnswerInt;
+    int tries = 0;
+
+#pragma endregion
+
 #pragma region Intro
 
     cout << "Hello i am " << narrator << "! \nWhat's your name ?" << endl;
-    string playerName;
+    
     cin >> playerName;
 
     cout << "Glad to see you " << playerName << " ! " << endl;
@@ -86,62 +140,60 @@ int main()
 
     DrawSeparator(separator);
 
-    cout << "Now your abilities :" << endl;
+    cout << "Abilities :" << endl;
 
-    map<string, int> abilities{
-        {"strength", RollAbility()},
-        {"dexterity", RollAbility()},
-        {"constitution", RollAbility()},
-        {"intelligence", RollAbility()}
-    };
-
-    cout << "   Strength : " << abilities["strenght"] << endl;
+    cout << "   Strength : " << abilities["strength"] << endl;
     cout << "   Dexterity : " << abilities["dexterity"] << endl;
     cout << "   Constitution : " << abilities["constitution"] << endl;
     cout << "   Intelligence : " << abilities["intelligence"] << endl;
 
     DrawSeparator(separator);
-    JumpLine();
 
 #pragma endregion
     
 #pragma region Class
 
-    if (abilities["strenght"] >= 12 || abilities["constitution"] >= 12) 
+    if ((abilities["strenght"] >= 30 && abilities["constitution"] >= 20) || (abilities["strenght"] >= 20 && abilities["constitution"] >= 30))
     {
-        cout << "Consider to become a Warrior" << endl;
+        cout << "TIP : Consider to become a Warrior" << endl;
     }
-    else if (abilities["intelligence"] <= 6) 
+    else if (abilities["intelligence"] <= 12)
     {
-        cout << "Consider to be a barabarian" << endl;
+        cout << "TIP : Consider to be a Barbarian" << endl;
     }
-    else
+    else if (abilities["intelligence"] >= 48)
     {
-        cout << "consider to be a bard" << endl;
+        cout << "TIP : consider to be a Mage" << endl;
+    }
+    else if (abilities["intelligence"] >= 33 && abilities["dexterity"] >= 33)
+    {
+        cout << "TIP : consider to be a Rogue" << endl;
     }
 
     JumpLine();
 
-    cout << "So now what do you want to become ?\n 1 - Warrior \n 2 - Rogue \n 3 - Mage \n 4 - Barbarian" << endl;
-    int playerClassChoice;
-    cin >> playerClassChoice;
+    playerAnswerInt = AskForInt("So now what do you want to become ?\n 1 - Warrior \n 2 - Rogue \n 3 - Mage \n 4 - Barbarian", "Answer with 1, 2, 3 or 4", 4);
 
-    switch (playerClassChoice)
+    switch (playerAnswerInt)
     {
     case 1:
         cout << "So " << playerName << " the Warrior, Ok" << endl;
+        playerClass = 'W';
         break;
     case 2:
         cout << "So " << playerName << " the Rogue, Ok" << endl;
+        playerClass = 'R';
         break;
     case 3:
         cout << "So " << playerName << " the Mage, Ok" << endl;
+        playerClass = 'M';
         break;
     case 4:
         cout << "So " << playerName << " the Barbarian, Ok" << endl;
+        playerClass = 'B';
         break;
     default:
-        cout << "You will be " << playerName << " the  bard" << endl;
+        cout << "That shouldn't happen" << endl;
         break;
     }
 
@@ -151,27 +203,16 @@ int main()
 
 #pragma region Bags
 
-    string smallbag[5] = { "Sword", "Shield", "Bow" };
-    vector<string> magicBag = { "Magic Hat", "Magic Wand", "Magic Cape" };
-
     cout << "You have 2 bags : a small one where you can put 5 items and a magic one where you can put how much you want but magics items." << endl << endl;
 
-    cout << "In your small bag you have :" << endl;
-    for (int i = 0; i < 5; i++) 
-    {
-        if (smallbag[i] != "")
-        {
-            cout << "\t -" << smallbag[i] << endl;
-        }
-    }
+    cout << "For the beggining in your small bag you have :" << endl;
+    DisplayBagArray(smallbag, 5);
 
     JumpLine();
 
-    cout << "In your magic bag you have :" << endl;
-    for (string item : magicBag) 
-    {
-        cout << "\t -" << item << endl;
-    }
+    cout << "For the beggining in your magic bag you have :" << endl;
+    DisplayBagVector(magicBag);
+    
 
 #pragma endregion
 
@@ -180,38 +221,30 @@ int main()
     DrawSeparator(separator);
 
     cout << "Now you are good to go on adventure. Are you ready ? 1 - Yes / 2 - No" << endl;
-    int playerAnswer;
-    cin >> playerAnswer;
 
-    while (playerAnswer != 1)
+    cin >> playerAnswerInt;
+
+    while (playerAnswerInt != 1)
     {
         cout << "C'mon you can do it ! 1 - Yes / 2 - No" << endl;
-        cin >> playerAnswer;
+        cin >> playerAnswerInt;
     }
 
     JumpLine();
-    cout << "Now we go" << endl;
+    
+    tries = 0;
 
-    do
+    if (AskForInt("And ... do you want a cookie before to go ? 1 - Yes / 2 - No", "Sure ? 1 - Yes / 2 - No", 2) == 1)
     {
-        cout << "Do you want a cookie before ? 1 - Yes / 2 - No" << endl;
-        cin >> playerAnswer;
-    } while (playerAnswer != 1);
+        magicBag.push_back("Magic Cookie");
+    }
+    
+
+    JumpLine();
+    cout << "Now we can go. GOOD LUCK" << endl;
 
 
 #pragma endregion
 
-
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
